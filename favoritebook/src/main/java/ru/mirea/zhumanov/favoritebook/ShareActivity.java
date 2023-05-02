228eafed25128a89de7f5af873f5c95b53caae39
Zhumanov Timur <zhumanov.timur2015@yandex.ru>
2023-05-02 19:22:37 +0300

    fixed bugs

--- favoritebook/src/main/java/ru/mirea/zhumanov/favoritebook/ShareActivity.java
+++ favoritebook/src/main/java/ru/mirea/zhumanov/favoritebook/ShareActivity.java
@@ -11,25 +11,25 @@ import android.widget.TextView;
 
 public class ShareActivity extends AppCompatActivity {
 
-    public EditText Editt;
+    public EditText Edit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share);
 
-        Editt = findViewById(R.id.editTextTextPersonName);
+        Edit = findViewById(R.id.editTextTextPersonName);
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
             TextView ageView = findViewById(R.id.textViewBook);
-            String university = extras.getString(MainActivity.KEY);
-            ageView.setText(String.format("Мой любимая книга: %s", university));
+            String favoriteBook = extras.getString(MainActivity.BOOK);
+            ageView.setText(String.format("Мой любимая книга: %s", favoriteBook));
         }
     }
 
     public void onClick(View v){
-        String text = String.valueOf(Editt.getText());
+        String text = String.valueOf(Edit.getText());
 
         Intent data = new Intent();
         data.putExtra(MainActivity.USER_MESSAGE, text);
